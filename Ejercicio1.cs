ec6e174156e540a198a31e7b8a0635ff76e53e5e
aenikka <63330374+aenikka@users.noreply.github.com>
2020-07-08 19:43:08 -0500

    Add files via upload

---
+++ Ejercicio1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Reto1
+{
+    class Program
+    {
+		public static void Main()
+		{
+			// Entrada del usuario de la variable X1
+			Console.Write("Ingrese X1: ");
+			double x1 = double.Parse(Console.ReadLine());
+
+			// Entrada del usuario de la variable Y1
+			Console.Write("Ingrese Y1: ");
+			double y1 = double.Parse(Console.ReadLine());
+
+			// Entrada del usuario de las variables restantes
+			// ...
+			Console.Write("Ingrese X2: ");
+			double x2 = double.Parse(Console.ReadLine());
+
+			// Entrada del usuario de la variable Y1
+			Console.Write("Ingrese Y2: ");
+			double y2 = double.Parse(Console.ReadLine());
+
+			double m = (y2 - y1) / (x2 - x1);
+			Console.WriteLine("La pendiente es: " + m);
+
+			double b = y1 - (m * x1);
+			Console.WriteLine("El intercepto es: " + b);
+
+			double distancia = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+			Console.WriteLine("La distancia es: " + distancia);
+
+
+		}
+	}
