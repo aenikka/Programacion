3a10d41111a1314a221c25a7b5af5f54477b643c
aenikka <63330374+aenikka@users.noreply.github.com>
2020-07-08 20:33:50 -0500

    Add files via upload

---
+++ Desafio 1 - triangulos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafío_1___Triángulos
+{
+    class Program
+    {
+		public static void Main()
+		{
+			Console.Write("Ingrese y y z: ");
+			double y = double.Parse(Console.ReadLine());
+			double z = double.Parse(Console.ReadLine());
+
+			double t = Math.Sqrt((z * z) + (y * y));
+			Console.WriteLine("El valor de t es: " + t);
+
+			double cRadianes = Math.Asin(z / t);
+			double cGrados = cRadianes * (180 / Math.PI);
+
+			Console.WriteLine("El valor de c es: " + cGrados);
+
+			double a = 180 - (90 + cGrados);
+			Console.WriteLine("El valor de a es: " + a);
+		}
+	}
+}
+
