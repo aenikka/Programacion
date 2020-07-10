fa46a4080d5e55155fdc87c1c1da1062f8d2cc8b
aenikka <63330374+aenikka@users.noreply.github.com>
2020-07-10 11:37:18 -0500

    Add files via upload

---
+++ Desafio 3 - 02 Triangulos dobles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio_3_02_Triangulos_dobles
+{
+    class Program
+    {
+		public static void Main()
+		{
+			Console.Write("Ingrese w, d en grados, x: ");
+			double w = double.Parse(Console.ReadLine());
+			double dGrados = double.Parse(Console.ReadLine());
+			double x = double.Parse(Console.ReadLine());
+
+			double baGrados = 180 - (dGrados + 90);
+			double baRadianes = baGrados * (Math.PI / 180);
+			double y = (Math.Sin(baRadianes) * w) - x;
+
+			Console.WriteLine("El valor de y es: " + y);
+
+		}
+	}
+}
