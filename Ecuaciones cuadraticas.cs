6d11998f3dae2f185ab8f068aec6a369ee375f68
aenikka <63330374+aenikka@users.noreply.github.com>
2020-07-16 16:59:15 -0500

    Add files via upload

---
+++ Ecuaciones cuadraticas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Ingrese los valores para a, b y c: ");
+		int a = int.Parse(Console.ReadLine());
+		int b = int.Parse(Console.ReadLine());
+		int c = int.Parse(Console.ReadLine());
+
+		double discriminante = (b * b) - 4 * a * c;
+
+		double solucionmenos = (-b - Math.Sqrt(discriminante)) / (2 * a);
+		double solucionmas = (-b + Math.Sqrt(discriminante)) / (2 * a);
+
+		if (discriminante == 0)
+			Console.WriteLine("Hay una sola solucion.");
+		Console.WriteLine("La solucion es: " + solucionmas);
+
+		if (discriminante > 0)
+			Console.WriteLine("Hay dos soluciones.");
+		Console.WriteLine("La solucion 1 es: " + solucionmas);
+		Console.WriteLine("La solucion 2 es: " + solucionmenos);
+
+		if (discriminante < 0)
+			Console.WriteLine("No existe solucion real.");
+
+	}
+}
---
+++ IMC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Ingrese su estatura en metros: ");
+		double estatura = double.Parse(Console.ReadLine());
+
+		Console.WriteLine("Ingrese su peso en kilos: ");
+		double peso = double.Parse(Console.ReadLine());
+
+
+		double imc = (peso / (estatura * estatura));
+
+		if (imc < 18.5)
+		{
+			Console.WriteLine("Está en un peso bajo");
+		}
+
+		else if (18.5 <= imc && imc <= 24.9)
+		{
+			Console.WriteLine("Está en un peso normal");
+		}
+
+		else if (25 <= imc && imc <= 29.9)
+		{
+			Console.WriteLine("Está en sobrepeso");
+		}
+
+		else
+		{
+			Console.WriteLine("Está en obesidad");
+		}
+
+		Console.WriteLine("Su índice de masa corporal es: " + imc);
+	}
+}
