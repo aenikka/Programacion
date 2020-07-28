91e06efd06a39a76ce98589dc887ec5fc78f9aad
aenikka <63330374+aenikka@users.noreply.github.com>
2020-07-28 13:37:34 -0500

    Add files via upload

---
+++ Blackjack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Random aleatorio = new Random();
+		int carta1 = 0, carta2 = 0, nuevacarta = 0, total = 0;
+		string continuar = "n";
+
+
+		carta1 = aleatorio.Next(1, 10);
+		carta2 = aleatorio.Next(1, 10);
+		Console.WriteLine("Carta 1 = " + carta1);
+		Console.WriteLine("Carta 2 = " + carta2);
+		total = carta1 + carta2;
+		Console.WriteLine("Total = " + total);
+
+		Console.Write("Desea tomar otra carta? (s/n): ");
+		continuar = Console.ReadLine();
+
+
+		while (continuar == "s")
+		{
+			nuevacarta = aleatorio.Next(1, 11);
+			Console.WriteLine("Carta = " + nuevacarta);
+			total += nuevacarta;
+			Console.WriteLine("Total = " + total);
+
+			if (total >= 21)
+			{
+				Console.WriteLine("Eliminado");
+				continuar = "n";
+			}
+
+			else { 
+			Console.Write("Desea tomar otra carta? (s/n): ");
+			continuar = Console.ReadLine();
+		}
+		}
+
+		Console.WriteLine("Su total fue " + total + " puntos");
+		Console.WriteLine("Gracias por jugar.");
+	}
+}
